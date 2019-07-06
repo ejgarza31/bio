ecff3b9adca1f9d446bbaafa479a1d5964a88041
EJ Garza <ejgarza31@gmail.com>
2019-07-06 16:07:45 -0400

    Add files via upload

---
+++ deblurExample.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#*****************************************************************************
+# Script that will execute common qiime2 commands to import paired-end reads,
+# join, filter, and denoise using deblur. No metadata is required for this
+# script. A manifest file must first be created.
+#
+# The next three lines are the input variables you should modify:
+#  * ROOT - is the directory where all output artifacts are saved. To start,
+#           ROOT dir should include the manifest file as well as this script.
+#  * PREFIX - is the name for the analysis/samples that gets prepended to
+#             each output artifact
+#  * MANIFEST - the filename of the manifest text file in the ROOT dir. The
+#               manifest can point to reads saved in a directory outside ROOT.
+#*****************************************************************************
+ROOT=/home/ejgarza/Qiime/Analysis
+PREFIX=corpusChristi
+MANIFEST=corpusChristiManifest.txt
+
+date | awk '{print $2" "$3" "$4}'
+module load bio/qiime2/2019.4
+
+echo "Importing reads using QIIME2"
+INPUT="$ROOT/corpusChristiManifest.txt"
+OUTPUT="$ROOT/corpusChristiDemux.qza"
+INPUTFORMAT="PairedEndFastqManifestPhred33V2"
+
+qiime tools import \
+  --type 'SampleData[PairedEndSequencesWithQuality]' \
+  --input-path $INPUT \
+  --output-path $OUTPUT \
+  --input-format $INPUTFORMAT
+
+echo "Joining using Vsearch"
+INPUT=$OUTPUT
+OUTPUT=$(echo $OUTPUT | sed 's/.qza$/Joined.qza/g')
+qiime vsearch join-pairs \
+  --i-demultiplexed-seqs $INPUT \
+  --o-joined-sequences $OUTPUT
+ 
+echo "Filtering"
+INPUT=$OUTPUT
+OUTPUT=$(echo $OUTPUT | sed 's/.qza$/Filtered.qza/g')
+OUTPUTSTATS=$(echo $OUTPUT | sed 's/.qza$/Stats.qza/g')
+qiime quality-filter q-score-joined \
+  --i-demux $INPUT \
+  --p-min-quality 4 \
+  --p-quality-window 3 \
+  --p-min-length-fraction 0.75 \
+  --p-max-ambiguous 0 \
+  --o-filtered-sequences $OUTPUT \
+  --o-filter-stats $OUTPUTSTATS \
+  --verbose
+
+echo "Denoising using Deblur"
+INPUT=$OUTPUT
+OUTPUT=$(echo $OUTPUT | sed 's/.qza$/Deblur.qza/g')
+OUTPUTTABLE=$(echo $OUTPUT | sed 's/.qza$/Table.qza/g')
+OUTPUTSTATS=$(echo $OUTPUT | sed 's/.qza$/Stats.qza/g')
+qiime deblur denoise-16S \
+  --i-demultiplexed-seqs $INPUT \
+  --p-trim-length 250 \
+  --p-jobs-to-start 12 \
+  --o-representative-sequences $OUTPUT \
+  --o-table $OUTPUTTABLE \
+  --p-sample-stats \
+  --o-stats $OUTPUTSTATS \
+  --output-dir $ROOT/denoise \
+  --verbose
+
+date | awk '{print $2" "$3" "$4}'
